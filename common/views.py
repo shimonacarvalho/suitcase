893f0042c4573a66bc4f5f69de6272e1db7e3376
Shimona Carvalho <shimona@truss.works>
2021-10-29 18:23:39 -0700

    Add API to register a user

--- common/views.py
+++ common/views.py
@@ -1,7 +1,20 @@
+from rest_framework import exceptions
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from common.serializers import UserSerializer
+
 
 # Create your views here.
 class RegisterAPIView(APIView):
     def post(self, request):
-        return Response('Hello world')
+        data = request.data
+
+        if data['password'] != data['password_confirm']:
+            raise exceptions.APIException('Passwords do not match!')
+
+        data['is_ambassador'] = False
+        serializer = UserSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
